d96d8926b0a3b7dcc6611c64f2ac8fc76af826d7
longpham <longpham3105@gmail.com>
2022-03-22 17:26:13 +0900

    Add setup.cfg

--- scripts/install_onecv_dev.sh
+++ scripts/install_onecv_dev.sh
@@ -3,7 +3,7 @@
 conda activate one
 
 # Absolute path to `one` dir:
-script_path=$(readlink -f "$0")     # workspaces/one/onecv/scripts/install_onecv.sh
+script_path=$(readlink -f "$0")     # workspaces/one/onecv/scripts/install_onecv_dev.sh
 dirpath=$(dirname "$script_path")   # workspaces/one/onecv/scripts/
 dirpath=$(dirname "$dirpath")       # workspaces/one/onecv/
 one=$(dirname "$dirpath")           # workspaces/one/
@@ -12,7 +12,7 @@ one=$(dirname "$dirpath")           # workspaces/one/
 echo "Install upgrade 'onecv'"
 onecv="${one}/onecv"  # workspaces/one/onecv
 cd "$onecv" || exit
-pip install ./ --upgrade
+pip install -e ./ --upgrade
 
 conda update --a --y
 conda clean --a --y
